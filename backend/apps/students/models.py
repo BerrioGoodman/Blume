fa76ea27492d181388488e2280c1448bfcfb36f4
BerrioGoodman <codeberrio@gmail.com>
2025-09-18 10:32:07 -0500

    chore: cambiando nombre de tablas por medio de model first

--- backend/apps/students/models.py
+++ backend/apps/students/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class EstudianteModel(models.Model): 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="students_estudiantemodel")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="estudiantes")
     carrera = models.CharField(max_length=200)
     semestre = models.IntegerField(default=1)
+
+    class Meta:
+        db_table = "estudiantes"
 
     def __str__(self):
         return self.user.username
